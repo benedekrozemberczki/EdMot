d20f45d84ca4adbd213c45471487384d5ccfae91
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-08-01 16:00:26 +0100

    Update edmot.py

--- src/edmot.py
+++ src/edmot.py
@@ -28,11 +28,17 @@ class EdMot(object):
         return len(set(nodes_1).intersection(set(nodes_2)))
 
     def _calculate_motifs(self):
+        """
+        Enumerating pairwise motif counts.
+        """
         print("\nCalculating overlaps.\n")
         edges = [edge for edge in tqdm(self.graph.edges()) if self._overlap(edge[0], edge[1]) >= self.cutoff]
         self.motif_graph = nx.from_edgelist(edges)
 
     def _extract_components(self):
+        """
+        Extracting connected components from motif graph.
+        """
         print("\nExtracting components.\n")
         components = list(nx.connected_component_subgraphs(self.motif_graph))
         components = [[len(c), c] for c in components]
@@ -42,12 +48,18 @@ class EdMot(object):
         
 
     def _fill_blocks(self):
+        """
+        Filling the dense blocks of the adjacency matrix.
+        """
         print("Adding edge blocks.\n")
         new_edges = [(node_1, node_2) for nodes in tqdm(self.blocks) for node_1 in nodes for node_2 in nodes]
         new_graph = nx.from_edgelist(new_edges)
         self.graph = nx.disjoint_union(self.graph,new_graph)
 
     def fit(self):
+        """
+        
+        """
         self._calculate_motifs()
         self._extract_components()
         self._fill_blocks()
